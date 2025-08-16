392b3c520d7bd523960d9434d5f7e9505c920b83
Adrian <adriandeemail@gmail.com>
2025-08-16 14:54:27 +0800

    feat：实现流式简单对话功能

--- src/main/java/com/adrian/aihub/controller/ChatController.java
+++ src/main/java/com/adrian/aihub/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * @Project: ai-hub
@@ -19,11 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ai")
 public class ChatController {
     private final ChatClient chatClient;
-    @RequestMapping("/chat")
-    public String chat(@RequestParam(defaultValue = "讲个笑话") String prompt) {
+
+    @RequestMapping(value = "/chat", produces = "text/html;charset=UTF-8")
+    public Flux<String> chat(@RequestParam(defaultValue = "讲个笑话") String prompt) {
         return chatClient
                 .prompt(prompt)
-                .call()
+                .stream()
                 .content();
     }
 }
