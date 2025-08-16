a8e815b4413176390e06c128d673c61b37d9cef0
Adrian <adriandeemail@gmail.com>
2025-08-16 14:23:05 +0800

    init：项目初始化

---
+++ src/main/java/com/adrian/aihub/AIHubApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.aihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrian/aihub/AIHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.aihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
